5f5da9f8fafee1ce036b791c950a356d4de73c1a
Keshav kumar <kumar.keshav4699@gmail.com>
2023-10-01 13:56:48 +0530

    habit seletion completed

--- controllers/habit_controller.js
+++ controllers/habit_controller.js
@@ -1,4 +1,5 @@
 const Habit = require("../models/habit_list");
+const Status = require("../models/habit_status");
 
 module.exports.add_habit = function (req, res) {
   try {
@@ -23,6 +24,11 @@ module.exports.create = async function (req, res) {
 module.exports.destroy = async function (req, res) {
   try {
     if (typeof req.query.id === "string") {
+      habit = await Habit.findById(req.query.id).populate('status');
+      for(let element of habit.status){
+        let id = element._id.toString();
+        await Status.findByIdAndDelete(id);
+      }
       await Habit.findByIdAndDelete(req.query.id);
     } else {
       for (let key in req.query.id) {
