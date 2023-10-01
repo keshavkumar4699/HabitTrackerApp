bec3e4e975a50d9d187988381d9742d340a04b68
Keshav kumar <kumar.keshav4699@gmail.com>
2023-10-02 01:06:24 +0530

    update-status-page created

--- routes/status_router.js
+++ routes/status_router.js
@@ -5,5 +5,7 @@ const statusController = require('../controllers/status_controller');
 
 router.get('/add_status', statusController.add_status);
 router.post('/create', statusController.create);
+router.get('/update_status', statusController.update_status);
+router.post('/update/:id', statusController.update);
 
 module.exports = router;
