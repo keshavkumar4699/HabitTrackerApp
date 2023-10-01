92f46c3191c4688285fd97a91315abfffbbf42f9
Keshav kumar <kumar.keshav4699@gmail.com>
2023-10-01 22:13:04 +0530

    status_controller updated

--- controllers/status_controller.js
+++ controllers/status_controller.js
@@ -1,54 +1,6 @@
 const Habit = require("../models/habit_list");
 const Status = require("../models/habit_status");
 
-// var calc_streak = function (status_date, habit, status) {
-//   var streak = habit.streak;
-
-//   //to get the date of todays date
-//   const today_date = new Date();
-//   //to get the date of current status date
-//   var curr_date = new Date(status_date); 
-
-//   var prev_date_status_doc = null; //to get the status document of previous date
-//   var next_date_status_doc = null; //to get the status document of next date
-
-//   if (status == "Done") { //in backward
-
-//     var temp_streak=0;
-//     var prev_date =  today_date;
-
-//     while (prev_date!=curr_date) {
-//       //find prev day status in status array of habit
-//       for (stat of habit.status) {
-//         if (stat.date.toDateString() == prev_date.toDateString()) {
-//           temp_streak++;
-//           break;
-//         } else {
-//           return temp_streak;
-//         }
-//       }
-//       var prev_date = new Date(prev_date - 864e5); //to get date of previous day from current date
-//     }
-
-//   } else if (status == "Not Done") { //in forward
-//     var temp_streak=0;
-//     var prev_date =  today_date;
-
-//     while (prev_date!=curr_date) {
-//       //find prev day status in status array of habit
-//       for (stat of habit.status) {
-//         if (stat.date.toDateString() == prev_date.toDateString()) {
-//           temp_streak++;
-//           break;
-//         } else {
-//           return temp_streak;
-//         }
-//       }
-//       var prev_date = new Date(prev_date - 864e5); //to get date of previous day from current date
-//     }
-//   }
-// };
-
 //function to calculate the current streak
 var calc_streak = function (habit) {
   const today_date = new Date();
