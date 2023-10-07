07153a973c8bbd0221b3bd95c108d3c2542d8edf
Keshav kumar <kumar.keshav4699@gmail.com>
2023-10-08 04:07:24 +0530

    set up port as environment variable

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require("express-ejs-layouts");
 const db = require("./config/mongoose");
+const dotenv = require('dotenv').config();
 
 //set the port for website
-const port = 8000; 
+const port = process.env.PORT;
 
 //setup static files folder
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 
 //setup express layouts
 app.use(expressLayouts);
 
-app.use(express.urlencoded());
+//to encode body in url after getting response
+app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
 
 //put scripts and styles from pages to layout
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
 
 //set the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 //setup express router
-app.use('/', require('./routes/index_router.js'));
+app.use("/", require("./routes/index_router.js"));
 
 //liten function
 app.listen(port, (err) => {
-  if(err){
-    console.log('******error encountered******');
+  if (err) {
+    console.log("******error encountered******");
     return;
   }
   console.log(`Server is up and running at port ${port}`);
